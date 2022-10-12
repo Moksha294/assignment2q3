0d7434c1a6e3f0e402cfaed86d516031bd385428
Moksha Shah <moksha.shah1@students.mq.edu.au>
2022-10-12 11:38:04 +1100

    Initial commit for the Multiserver package

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Bank Data
+#'
+#' This dataset simulates customer arrival and service times in a bank.
+#' The bank data frame has 100 variables
+#' on 2 variables
+#'
+#' @name bank
+#' @format A data frame with 100 observations on 2 variables
+#' \describe{
+#' \item{arrival_time}{Interval Arrival time for each customer}
+#' \item{service_time}{Service time for each customer}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @usage
+#' data(bank)
+#' @examples
+#' ## see example(Multiserver)
+NULL
